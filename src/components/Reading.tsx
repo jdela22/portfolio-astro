95421db7085ff66ca8cefd436e16f4339a38221e
Jared <jareddelapasse@gmail.com>
2026-01-27 23:01:49 -0600

    header/sidebar

---
+++ src/components/Reading.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+export const Reading = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      {count}
+      <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
+    </>
+  );
+};
